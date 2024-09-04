3d34ef58e55c2fb1147fb740e48420e677a101bc
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2024-09-04 22:38:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 let slideIndex = 0;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
 function showSlides(n) {
+    let i;
     let slides = document.getElementsByClassName("slide");
     if (n >= slides.length) {slideIndex = 0}
     if (n < 0) {slideIndex = slides.length - 1}
-    for (let i = 0; i < slides.length; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex].style.display = "block";
 }
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
 showSlides(slideIndex);
 
-// Initialize AOS (Animate On Scroll) Library
+// Initialize AOS
 AOS.init();
