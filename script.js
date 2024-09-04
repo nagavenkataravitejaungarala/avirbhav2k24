d1c4901347dccafb8fe8587ddb62215382535e25
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2024-09-04 19:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+showSlides(slideIndex);
+
+// Initialize AOS (Animate On Scroll) Library
+AOS.init();
